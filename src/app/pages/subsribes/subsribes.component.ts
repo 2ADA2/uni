5d61629850db50c56663e33e78012303eb35a27e
ada2 <87185550+2ADA2@users.noreply.github.com>
2024-12-05 00:30:08 +0300

    uni v0.0.2

---
+++ src/app/pages/subsribes/subsribes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subsribes',
+  standalone: true,
+  imports: [],
+  templateUrl: './subsribes.component.html',
+  styleUrl: './subsribes.component.scss'
+})
+export class SubsribesComponent {
+
+}
